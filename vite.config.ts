233e7a2a23d386bdb8e5ac1da91df107b06434da
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2024-11-18 23:29:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['uuid'],
-    },
+    outDir: 'dist', // Ensure this is set to 'dist'
   },
 });
